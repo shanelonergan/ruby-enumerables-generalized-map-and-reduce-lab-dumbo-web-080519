cc4584ac829fd16ceaac52cca302b4041171636e
Shane Lonergan <sptlonergan@gmail.com>
2019-07-26 22:11:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# Your Code Here
+def my_own_map(source_array)
+  new_array = []
+  new_array = source_array.each { |n| new_array << yield(n) }
+  return new_array
+end
+
+def my_own_reduce
+  source_array.each {|n| }
+end
