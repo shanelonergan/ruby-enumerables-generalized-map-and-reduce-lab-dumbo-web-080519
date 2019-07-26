17bbeede4b63cbb7d5ff44e0324b259f0fd2a965
Shane Lonergan <sptlonergan@gmail.com>
2019-07-26 23:16:23 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
-# Your Code Here
+def map(source_array)
+  new_array = []
+  source_array.each { |n| new_array.push(yield(n)) }
+  return new_array
+end
+
+def reduce(source_array, starting_value = nil)
+  if starting_value 
+    value = starting_value
+    i = 0
+  else
+    value = source_array[0]
+    i = 1
+  end
+  
+  while i < source_array.length do
+    value = yield(value, source_array[i])
+    i += 1
+  end
+
+  return value
+end
