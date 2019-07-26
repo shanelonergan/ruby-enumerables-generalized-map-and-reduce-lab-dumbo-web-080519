9a4fa776bae4a2e685fcd086c5119d9230362d61
Shane Lonergan <sptlonergan@gmail.com>
2019-07-26 23:14:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,12 +4,19 @@ def map(source_array)
   return new_array
 end
 
-def reduce(source_array, starting_value = 0)
-  i = 0
+def reduce(source_array, starting_value = nil)
+  if starting_value 
+    value = starting_value
+    i = 0
+  else
+    value = source_array[0]
+    i = 1
+  end
+  
   while i < source_array.length do
-    value = yield(source_array[i])
+    value = yield(value, source_array[i])
     i += 1
-    return value
   end
 
+  return value
 end
